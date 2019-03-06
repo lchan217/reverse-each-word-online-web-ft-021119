38da01cb9127af9c016352db1408dd12196230e0
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:35:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,12 @@ def reverse_each_word(string)
   end
   array.join(" ")
 end
+
+# def reverse_each_word(sentence)
+#   new_array=[]
+#   array1 = sentence.split(" ")
+#   array1.each do |x|
+#       new_array << x.reverse
+#   end
+#   new_array.join(" ")
+# end
