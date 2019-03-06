bd0c624a0c04fdc0470930d082fb5a4ef18e0e85
Lizzie Chan <lchan217@gmail.com>
2019-03-06 01:35:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,2 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  array.collect do |element|
-    element.reverse!
-  end
-  array.join(" ")
-end
-
-# def reverse_each_word(sentence)
-#   new_array=[]
-#   array1 = sentence.split(" ")
-#   array1.each do |x|
-#       new_array << x.reverse
-#   end
-#   new_array.join(" ")
-# end
+def reverse_each_word(array)
+end 
